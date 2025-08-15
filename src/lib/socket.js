e54844a2d2e2ff386cd689207df8be979da10011
stanli206 <stantilin@gmail.com>
2025-08-15 15:33:15 +0530

    `Update socket.js to allow connections from production frontend URL`

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -7,9 +7,9 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["https://fullstack-chatapp-fe.netlify.app"],
   },
-});
+}); ///http://localhost:5173
 
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
