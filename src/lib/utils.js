912c6851576635b230d29a2bf2e974ffda4ed722
stanli206 <stantilin@gmail.com>
2025-08-15 15:54:01 +0530

    `Update cookie settings in generateToken function: changed sameSite to "none" and removed secure flag`

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -7,8 +7,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development",
+    sameSite: "none",
   });
   return token;
-};
+}; //strict
