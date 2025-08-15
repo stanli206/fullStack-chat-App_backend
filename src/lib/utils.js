b90298924a0b5ba01d4264315d0a71620e3d11b8
stanli206 <stantilin@gmail.com>
2025-08-15 16:06:36 +0530

    `Update cookie settings in generateToken function`

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,10 +4,16 @@ export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
+  // res.cookie("jwt", token, {
+  //   maxAge: 7 * 24 * 60 * 1000,
+  //   httpOnly: true,
+  //   sameSite: "none",
+  // });
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 1000,
     httpOnly: true,
-    sameSite: "none",
+    secure: process.env.NODE_ENV === "production", // true only in production (HTTPS)
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // none for cross-site in prod
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
   return token;
 }; //strict
